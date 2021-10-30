bdb2348f788af62002c699354f3c32c92ba5e227
Santiago <santi.codev@gmail.com>
2021-10-30 13:33:52 +0200

    initial commit

---
+++ src/main/java/com/santicodev/demospringwebthymeleaf/DemospringwebthymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.santicodev.demospringwebthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringwebthymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringwebthymeleafApplication.class, args);
+	}
+
+}
